8a4939c811f3d386ab0ddd82b5b8125f7b4f36fa
DharshiniSathasivam <dharshinisatha54@gmail.com>
2023-08-16 20:50:14 +0530

    fuction

---
+++ script.js
@@ -0,0 +1,30 @@
+//function functionname(parameter)
+// {
+     //set of statements
+// }
+//functionname();//function call
+//function always have return value
+//DRY (dont repeat yourself)
+//-----------------------------------------------------
+// function mul(a,b){
+   //console.log(a*b);
+//     return a*b;//1 fun only 1 return keyword
+// }
+// function add(a,b,c){
+//     return a+b+c;
+// }
+// function sub(a,b){
+//     return a-b;
+// }
+// function div(a,b){
+//     return a/b;
+// }
+// function md(a,b){
+//     return a%b;
+// }
+// console.log(add(2,5,10));
+// console.log(sub(2,5));
+// console.log(mul(2,5));
+// console.log(div(2,5));
+// console.log(md(2,5,));
+//------------------------------------------------------
